a661fb4366d19b018bdb7791c4c694997f4e09dc
yash-2115 <mailforyash04@gmail.com>
2021-11-11 23:24:33 +0530

    Day 9 done

--- 09 Day 9 - Beginner - Dictionaries, Nesting and the Secret Auction/05 blind Action.py
+++ 09 Day 9 - Beginner - Dictionaries, Nesting and the Secret Auction/05 blind Action.py
@@ -1,29 +1,29 @@
 from replit import clear
 from art import logo
-
 print(logo)
 
-data ={}
-bidding_stop = False
+bids = {}
+bidding_finished = False
+
 
 def find_highest_bidder(bidding_record):
     highest_bid = 0
-
+    winner = ""
+    # bidding_record = {"Angela": 123, "James": 321}
     for bidder in bidding_record:
-        bid_ammount = bidding_record[bidder]
-        if bid_ammount > highest_bid:
-            highest_bid = bid_ammount
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
             winner = bidder
-        print(f"The winner is {winner} with bid of ${highest_bid}")
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
 
-while not bidding_stop:
-    name = str(input("What is your name: "))
-    bid_price = int(input("Please enter bid price: $"))
-    data[name] = bid_price
-    ask = str(input("Are there any other bidders? Type 'yes' or 'no'.\n"))
 
-    if ask == "no":
-        bidding_stop = True
-        find_highest_bidder(data)
-    elif ask == "yes":
-        clear()
+while not bidding_finished:
+    name = input("What is your name?: ")
+    price = int(input("What is your bid?: $"))
+    bids[name] = price
+    should_continue = input(
+        "Are there any other bidders? Type 'yes or 'no'.\n")
+    if should_continue == "no":
+        bidding_finished = True
+        find_highest_bidder(bids)
